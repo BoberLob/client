ca6c240a1b7537d840c2a166cac1a236bad5bc1d
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-12-16 20:59:13 +0200

    add spinner

---
+++ src/components/Spiner/spiner.js
@@ -0,0 +1,13 @@
+export default class Spinner {
+  constructor(container) {
+    this.container = container;
+  }
+
+  handleSpinner = (isLoading) => {
+    if (isLoading) {
+      this.container.classList.add("spinner_visible");
+    } else {
+      this.container.classList.remove("spinner_visible");
+    }
+  };
+}
---
+++ src/components/app.js
@@ -0,0 +1,4 @@
+import Spinner from "./Spiner/Spiner";
+// Спиннер
+const handleSpinner = new Spinner(document.querySelector(".spinner"))
+  .handleSpinner;
