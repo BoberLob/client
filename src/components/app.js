44932a97960230a4520066e14e74e8af3111c33f
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-12-16 21:51:21 +0200

    add spinner

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,6 @@
-import Spinner from "./Spiner/Spiner";
+import "./app.css";
+import Spinner from "./Spinner/Spinner.js";
 // Спиннер
 const handleSpinner = new Spinner(document.querySelector(".spinner"))
   .handleSpinner;
+handleSpinner(true);
